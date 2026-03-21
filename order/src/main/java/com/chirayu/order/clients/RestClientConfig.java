eae67be6f1ebe0aa0a3093c5761b94e53c0e11b5
ChirayuP19 <chirayu010675@gmail.com>
2026-03-21 13:16:42 -0400

    feat(tracing): integrate Zipkin distributed tracing with RestClient
    - Added Micrometer tracing with Zipkin exporter
    - Configured RestClient with LoadBalancer support
    - Implemented custom ClientHttpRequestInterceptor for trace propagation
    - Injected trace context using Propagator to maintain traceId across services

--- order/src/main/java/com/chirayu/order/clients/RestClientConfig.java
+++ order/src/main/java/com/chirayu/order/clients/RestClientConfig.java
@@ -1,16 +1,44 @@
 package com.chirayu.order.clients;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
+
+
+    private final ObservationRegistry observationRegistry;
+    private final Tracer tracer;
+    private final Propagator propagator;
 
     @Bean
     @LoadBalanced
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+        if(observationRegistry !=null){
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+        return builder;
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return ((request, body, execution) -> {
+            if(tracer !=null && propagator != null && tracer.currentSpan() != null){
+                propagator.inject(tracer.currentTraceContext().context(),
+                        request.getHeaders(),
+                        (carrier,key,value)
+                                -> carrier.add(key,value));
+            }
+            return execution.execute(request,body);
+        }
+        );
     }
 }
